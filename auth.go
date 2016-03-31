345bf342c4f9ec1a07f586ad3cfa809645a80a0b
Jordan Anderson <me@jba.io>
2016-03-30 21:40:33 -0500

    still trying to get CLI working again...trying user-agent specific, but this is really open to abuse :(

--- auth.go
+++ auth.go
@@ -820,13 +820,12 @@ func XsrfMiddle(next http.Handler) http.Handler {
                 utils.Debugln("POST: tmplToken: "+tmplToken)
                 // Actually check CSRF token, since this is a POST request
                 if tmplToken == "" {
-                    contentType := r.Header.Get("Content-Type")
-                    log.Println(contentType)
+                    log.Println(r.UserAgent())
                     // This should mean this is a request from the command line, so don't check CSRF
-                    if contentType == "" {
+                    /*if contentType == "" {
                         next.ServeHTTP(w, r)
                         return
-                    }
+                    }*/
                     http.Error(w, "CSRF Blank.", 500)
                     utils.Debugln("**CSRF blank**")
                     return
