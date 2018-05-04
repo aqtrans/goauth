c8e8c041f5aea985f30f324c683141def6234274
Jordan Anderson <me@jba.io>
2018-05-03 23:04:52 -0500

    Integrated the handlers into the lib itself. Same functionality!

--- oauth/main.go
+++ oauth/main.go
@@ -15,8 +15,8 @@ func main() {
 
 	auth.Debug = true
 	http.HandleFunc("/", handleMain)
-	http.HandleFunc("/login", handleGoogleLogin)
-	http.HandleFunc("/callback", handleGoogleCallback)
+	http.HandleFunc("/login", userstate.GoogleLogin)
+	http.HandleFunc("/callback", userstate.GoogleCallback)
 	log.Println("listening on port 3000")
 	err := http.ListenAndServe("127.0.0.1:3000", nil)
 	if err != nil {
@@ -39,44 +39,5 @@ func handleMain(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, `<html><body>
 			<a href="/login">Log in with Google</a>
 			</body></html>`)
-	return
-}
-
-func handleGoogleLogin(w http.ResponseWriter, r *http.Request) {
-	b := auth.RandString(8)
-	userstate.SetState(b, w)
-
-	url := userstate.GetLoginURL(b)
-	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
-}
-
-func handleGoogleCallback(w http.ResponseWriter, r *http.Request) {
-
-	state := r.FormValue("state")
-	expectedState := userstate.ReadState(w, r)
-
-	if state != expectedState {
-		log.Println("state and expectedState do not match.", state, expectedState)
-		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-		return
-	}
-
-	code := r.FormValue("code")
-	username, nonce, err := userstate.VerifyUser(code)
-	if nonce != expectedState {
-		log.Println("Nonce does not match!")
-	}
-	if err != nil {
-		log.Println("Error verifying user:", err)
-		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-		return
-	}
-
-	userstate.SetUsername(username, w)
-
-	// Set the ID token into the "token" securecookie
-	//userstate.SetToken(rawIDToken, w)
-
-	http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 	return
 }
